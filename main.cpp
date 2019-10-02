2cd52e504891c669e7c55654b3d9fb6d2b956ffc
awlada <awlada@mail.csuchico.edu>
2019-10-02 10:01:42 -0700

    Blank Screen when run

--- main.cpp
+++ main.cpp
@@ -14,17 +14,25 @@ int main()
   getline(cin, fileName);
   
   //STEP 1: open the fileStream for input, using the fileName specified
-  
-  if( /*STEP 2: check to see if the fileStream successfully opened*/ )
+fileStream.open("fileName",ios::in);
+
+
+
+  if(fileStream.in) /*STEP 2: check to see if the fileStream successfully opened*/
   {
     cout<<fileName<<" opened.\nFILE CONTENTS:\n";
     
     //STEP 3: repeat the following until the end-of-file (eof) has been reached...
-    // 3A: read a line from fileStream into the variable line
-    // 3B: display the line, followed by an endline
+    while (fileStream.in && !fileStream.eof())// 3A: read a line from fileStream into the variable line
+    {
+            getline(fileStream,line);
+            cout<<line<<endl;
+    }
+
+            // 3B: display the line, followed by an endline
   
     //STEP 4: close the fileStream
-  
+  fileStream.close();
   }
   else
   {
