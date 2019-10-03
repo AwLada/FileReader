d385525de6a528297a466e276376db47abb40e56
awlada <awlada@mail.csuchico.edu>
2019-10-02 17:10:53 -0700

    working lab 14

--- main.cpp
+++ main.cpp
@@ -6,42 +6,59 @@ using namespace std;
 
 int main()
 {
+char last;
+
+do
+ {
   string fileName; //to get the name of the file to open
   string line; //to store a single line of a text file
   fstream fileStream; //to open a file for reading
-
+  int number = 0;
+  int lineLength = 0;
+ 
   cout<<"What file do you want to open? ";
   
   getline(cin, fileName);
   
   fileStream.open(fileName,ios::in);
 
-
-  //
   if    (fileStream.is_open())
-
   {
    cout<<fileName<<" opened.\nFILE CONTENTS:\n";
-    
-   //
+  
    while (fileStream.is_open() && !fileStream.eof())
    {
             getline(fileStream,line);
             cout<<line<<endl;
-            //fileStream.close();
+            
+            if (!line.empty())
+            {
+                number += 1;          
+                lineLength += line.length();
+            }
    }
    
-   //
    fileStream.close();
-                                                                               // 3B: display the line, followed by an endline
-  
+  }                                                                             // 3B: display the line, followed by an endline
+  //cout<<"METADATA"<< endl << "File: " << fileName << endl << "Lines: "<< number <<endl << "Characters: " << lineLength <<endl;
                                                                                          //STEP 4: close the fileStream
- 
-  }
   else
   {
     cout<<fileName<<" could not be opened.\n";
   }
-
+  cout<<"METADATA"<<endl;
+  cout<<"File: "<<fileName<<endl;
+  cout<<"Lines: "<<number<<endl;
+  cout<<"Characters: "<<lineLength<<endl;
+  cout<<"Analyze another file (y/n)?"<<endl;
+  cin>>last;
+  cin.ignore();
+}
+  while(toupper (last) == 'Y');
+  
+    
+ 
+  
+  
   return 0;
 }
